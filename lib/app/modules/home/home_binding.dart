efa244971f221dc0b2988af83a69e508d23802d2
Aldo Johanda <johandaaldo00@gmail.com>
2023-04-06 11:14:36 +0700

    a

--- lib/app/modules/home/home_binding.dart
+++ lib/app/modules/home/home_binding.dart
@@ -3,7 +3,5 @@ import 'package:mind_pal_app/app/modules/home/home_controller.dart';
 
 class HomeBinding implements Bindings {
   @override
-  void dependencies() {
-    Get.put(HomeController());
-  }
+  void dependencies() {}
 }
