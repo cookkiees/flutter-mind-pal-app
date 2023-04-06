7abb10301ad7333bef59bc40025e5840aea612ce
Aldo Johanda <johandaaldo00@gmail.com>
2023-04-06 11:41:22 +0700

    Add bottomnavigationbar

--- lib/app/utils/my_colors.dart
+++ lib/app/utils/my_colors.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class MyColors {
   static const purple = Color(0xFF5620CC);
   static const neutralGrey = Color(0xFF666666);
+  static const neutralLightGrey = Color(0xFF9E9E9E);
   static const neutraldDark = Color(0xFF181616);
 }
