6a8c6696d76f03f12841a0434fd1c546dd293242
Aldo Johanda <johandaaldo00@gmail.com>
2023-04-06 10:28:07 +0700

    Add onboading screen and add componnets

--- lib/app/routings/app_pages.dart
+++ lib/app/routings/app_pages.dart
@@ -1,12 +1,17 @@
-
 import 'package:get/get.dart';
+import 'package:mind_pal_app/app/modules/onboarding/onboarding_binding.dart';
 
+import '../modules/onboarding/onboarding_screen.dart';
 import 'app_routers.dart';
+
 class AppPages {
-  static const initial = AppRouters.;
+  static const initial = AppRouters.onboarding;
 
   static final routes = [
-    GetPage(name: AppRouters.onboarding, page: () => const OnboardingScreen()),
-
+    GetPage(
+      name: AppRouters.onboarding,
+      page: () => OnboardingScreen(),
+      binding: OnboardingBinding(),
+    ),
   ];
 }
